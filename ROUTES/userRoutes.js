3365fe209d4c303af4e702dafd0800a455e6b676
student nikhildemo <nikhilshinde2257@gmail.com>
2026-02-04 17:43:15 +0530

    verification route added

--- ROUTES/userRoutes.js
+++ ROUTES/userRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const userRouter = express.Router();
 const {createorder}= require('../controller/createOrder');
+const {verifyPayment} = require('../controller/verifyPayment');
 
 //prefix-endpoint user
 userRouter.post('/create-order',createorder);
+userRouter.post("/verify-payment",verifyPayment);
 
 module.exports = {userRouter};
 
